dd825726d2e0a29aaed4101cf2991acb5149a464
olorunyomi97 <olorunyomi97@gmail.com>
2022-01-12 01:58:29 +0100

    Travel Home Page

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -2,12 +2,18 @@ import React, { useState, useRef } from 'react';
 import { View, Text, TouchableOpacity, Image,  ScrollView, SafeAreaView, Platform, Animated } from 'react-native';
 
 import { dummyData, COLORS, SIZES, FONTS, icons, images } from "../constants"
+import { TextButton } from '../component';
 
 const COUNTRIES_ITEM_SIZE = SIZES.width / 3
+const PLACES_ITEM_SIZE = Platform.OS === 'ios' ? SIZES.width / 1.25 : SIZES.width / 1.2
+const EMPTY_ITEM_SIZE = (SIZES.width - PLACES_ITEM_SIZE) / 2  
 const Dashboard = ({ navigation }) => {
 
     const countryScrollX = useRef(new Animated.Value(0)).current;
+    const placesScrollX = useRef(new Animated.Value(0)).current;
+
     const [countries, setCountries] = useState([{id: -1}, ...dummyData.countries, {id: -2}])
+    const [places, setPlaces] = useState([{id: -1}, ...dummyData.countries[0].places, {id: -2}])
     // Function for render Header //
     function renderHeader() {
         return (
@@ -25,7 +31,7 @@ const Dashboard = ({ navigation }) => {
                         width:45,
                         height:45,
                         alignItems: 'center',
-                        justifyContent: 'centet'
+                        justifyContent: 'center'
                     }}
                     onPress={() => console.log("Side Drawer")}
                 >
@@ -76,7 +82,7 @@ const Dashboard = ({ navigation }) => {
         )
     }
 
-// Function for country List
+    // Function for country List
     function renderCountries() {
         return (
             <Animated.FlatList
@@ -92,6 +98,16 @@ const Dashboard = ({ navigation }) => {
                 onScroll={Animated.event([
                     { nativeEvent: { contentOffset: { x: countryScrollX }}}
                 ], { useNativeDriver: false })}
+                onMomentumScrollEnd={(event) => {
+                    // Calculate Position
+                    var position = (event.nativeEvent.contentOffset.x / COUNTRIES_ITEM_SIZE).toFixed(0)
+                    // Set Place
+                    setPlaces([
+                        {id: -1},
+                        ...dummyData.countries[position].places,
+                        {id: -2}
+                    ])
+                }}
                 renderItem={({item, index}) => {
                     const opacity = countryScrollX.interpolate({
                         inputRange: [
@@ -172,6 +188,136 @@ const Dashboard = ({ navigation }) => {
         )
     }
 
+    // Function for Places
+    function renderPlaces() {
+        return (
+            <Animated.FlatList 
+                horizontal
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+                data={places}
+                keyExtractor={item => `${item.id}`}
+                contentContainerStyle={{
+                    alignItems: 'center'
+                }}
+                snapToAlignment='center'
+                snapToInterval={Platform.OS === 'ios' ? PLACES_ITEM_SIZE + 28 : PLACES_ITEM_SIZE}
+                scrollEventThrottle={16}
+                decelerationRate={0}
+                bounces={false}
+                onScroll={Animated.event([
+                    { nativeEvent: { contentOffset: { x: placesScrollX }}}
+                ], { useNativeDriver: false })}
+                renderItem={({item, index}) => {
+
+                    const opacity = placesScrollX.interpolate({
+                        inputRange: [
+                            (index - 2) * PLACES_ITEM_SIZE,
+                            (index - 1) * PLACES_ITEM_SIZE,
+                            index * PLACES_ITEM_SIZE,
+                        ],
+                        outputRange : [0.3, 1, 0.3],
+                        extrapolate: 'clamp'
+                    })
+
+                    if (Platform.OS === 'ios') {
+                        if (SIZES.height > 800) {
+                            activeHeight = SIZES.height / 2
+                        } else {
+                            activeHeight = SIZES.height / 1.65
+                        }
+                    } else {
+                        activeHeight = SIZES.height / 1.6
+                    }
+
+                    const height = placesScrollX.interpolate({
+                        inputRange: [
+                            (index - 2) * PLACES_ITEM_SIZE,
+                            (index - 1) * PLACES_ITEM_SIZE,
+                            index * PLACES_ITEM_SIZE,
+                        ],
+                        outputRange : [SIZES.height / 2.25, activeHeight, SIZES.height / 2.25],
+                        extrapolate: 'clamp'
+                    })
+
+                    if(index == 0 || index == places.length - 1) {
+                        return (
+                            <View 
+                                style={{
+                                    width: EMPTY_ITEM_SIZE
+                                }}
+                            />
+                        )
+                    } else {
+                        return(
+                            <Animated.View
+                                opacity={opacity}
+                                style={{
+                                    width: PLACES_ITEM_SIZE,
+                                    height: height,
+                                    alignItems:'center',
+                                    borderRadius: 20,
+                                    padding: 10
+                                }}
+                            >
+                                <Image 
+                                    source={item.image}
+                                    resizeMode='cover'
+                                    style={{
+                                        position: 'absolute',
+                                        width: '100%',
+                                        height: '100%',
+                                        borderRadius: 20,
+                                    }}
+                                />
+
+                                <View
+                                    style={{ 
+                                        flex : 1,
+                                        alignItems: 'center',
+                                        justifyContent: 'flex-end',
+                                        marginHorizontal: SIZES.padding
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            marginBottom: SIZES.radius,
+                                            color: COLORS.white,
+                                            ...FONTS.h1
+                                        }}
+                                    >
+                                        {item.name}
+                                    </Text>
+                                    <Text
+                                        style={{
+                                            marginBottom: SIZES.padding * 2,
+                                            textAlign:'center',
+                                            color: COLORS.white,
+                                            ...FONTS.body3
+                                        }}
+                                    >
+                                        {item.description}
+                                    </Text>
+                                    <TextButton 
+                                        label="Explore"
+                                        customContainerStyle={{
+                                            position: 'absolute',
+                                            bottom: -20,
+                                            width: 150
+                                        }}
+                                    />
+                                </View>
+                            </Animated.View>
+                        )
+                    }
+
+                    
+                    
+                }}
+            />
+        )
+    }
+
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.black}}>
             {renderHeader()}
@@ -187,6 +333,9 @@ const Dashboard = ({ navigation }) => {
                         {renderCountries()}                        
                     </View>
                     {/* Places */}
+                    <View style={{height: Platform.OS === 'ios' ? 500 : 450 }}>
+                        {renderPlaces()}
+                    </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
