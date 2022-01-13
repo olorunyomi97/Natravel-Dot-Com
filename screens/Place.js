5060f003335bb236b9cf4eafa3531b1469abdc03
olorunyomi97 <olorunyomi97@gmail.com>
2022-01-13 11:07:11 +0100

    Travel App Page Navigation

--- screens/Place.js
+++ screens/Place.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, ImageBackground, Animated } from 'react-native';
+import { COLORS, SIZES, FONTS, icons } from "../constants"
 
-const Place = () => {
+
+const Place = ({navigation, route}) => {
+
+        const [selectedPlace, setSelectedPlace] = React.useState(null);
+    React.useEffect(() => {
+        let {selectedPlace} = route.params;
+        setSelectedPlace(selectedPlace)
+    }, [])
+
+    // Function For Selected Place
+    function renderPlace() {
+        return (
+            <ImageBackground
+                source={selectedPlace?.image}
+                style={{
+                    width:'100%',
+                    height:'100%'
+                }}
+            >
+
+            </ImageBackground>
+        )
+    }
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Place</Text>
+            {renderPlace()}
         </View>
     )
 }
